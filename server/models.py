5afa688443eca6f602f5796a9ced89b7b54179cd
Julie Cheng <jucheng925@gmail.com>
2023-12-31 16:13:53 -0500

    struck on constrainting instructions minimum length

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import validates
+# from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
@@ -8,7 +8,7 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
@@ -32,13 +32,13 @@ class Recipe(db.Model, SerializerMixin):
     user_id= db.Column(db.Integer, db.ForeignKey('users.id'))
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    instructions = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, db.CheckConstraint('func.length(recipes.instructions) > 49'), nullable=False)
     minutes_to_complete = db.Column(db.Integer)
 
     user = db.relationship('User', back_populates='recipes')
 
-    @validates('instructions')
-    def validate_instructions(self, key, instructions):
-        if len(instructions) < 50:
-            raise AttributeError("Instructions require a minimum of 50 characters")
-        return instructions
+    # @validates('instructions')
+    # def validate_instructions(self, key, instructions):
+    #     if len(instructions) < 50:
+    #         raise ValueError("Instructions require a minimum of 50 characters")
+    #     return instructions
